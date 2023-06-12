9b3bfbbbe3fe39f3d984d44a5f5514ff91ebf7da
Sumner Evans <me@sumnerevans.com>
2023-06-12 12:16:20 -0600

    shell: convert to flakes
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Mines HSPC Website";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { system = system; };
+        in
+        rec {
+          devShells = {
+            default = pkgs.mkShell {
+              packages = with pkgs; [
+                gnumake
+                nodePackages.html-minifier
+                nodePackages.uglify-js
+                sass
+                rnix-lsp
+              ];
+            };
+          };
+        }
+      ));
+}
